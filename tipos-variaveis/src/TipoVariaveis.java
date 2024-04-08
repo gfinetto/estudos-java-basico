f8f9b6e8be5d29ec4a7e84d7ce411eb3afd9bbc5
Gabriel Finetto <gabriel-finetto@hotmail.com>
2024-04-08 01:17:50 -0300

    variaiveis 1

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,16 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(numero);
+
     }
 }
