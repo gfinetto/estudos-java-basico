fb20bac6f6aee914d3bf4aee60af9ca9393c5791
Gabriel Finetto <gabriel-finetto@hotmail.com>
2024-04-08 15:56:39 -0300

    concatenacao

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,24 +1,17 @@
 public class Operadores {
     
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.println(nomeCompleto);
+        int numero = 5;
+        //x repetição 
+        numero++;~
+        //numero = numero + 1;
+        
 
-        String concatenacao ="?"; 
+        System.out.println(numero);;
 
-        concatenacao = 1+1+1+"1";
-            System.out.println(concatenacao);
 
-        concatenacao = 1+"1"+1+1;
-            System.out.println(concatenacao);
 
-        concatenacao = 1+"1"+1+"1";
-            System.out.println(concatenacao);
 
-        concatenacao = "1"+1+1+1;
-            System.out.println(concatenacao);
 
-        concatenacao = "1"+(1+1+1);
-            System.out.println(concatenacao);
     }
 }
