59bcbd367eba60a02569a5efbb950c756377573c
Gabriel Finetto <gabriel-finetto@hotmail.com>
2024-04-11 02:06:47 -0300

    Estrutura de repetição -  ForArray

---
+++ estruturas-repetição/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray{
+    public static void main(String[] args) {
+        
+        //em arrays o indice inicia com zero
+        String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+       for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+       }
+    }
+    }
